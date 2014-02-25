24a46221f3667c70f64d4ff17d8aa596c4a5ae63
bellmast <jbm444@gmail.com>
2014-02-25 13:06:55 -0600

    Update IncViz.js

--- IncViz.js
+++ IncViz.js
@@ -22,7 +22,7 @@ function drawNetwork(data) {
     orgXcoords = []
     orgSet = paper.set();
     textSet = paper.set();
-    orgColors = ["#03899C", "#1240AB", "#WFFAA00", "#FF7A00"]
+    orgColors = ["#03899C", "#1240AB", "#FFAA00", "#FF7A00"]
     
     slots = canvasWidth/(orgNumber+1)
     midY = canvasHeight/2
