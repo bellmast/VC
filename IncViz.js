099571948f0a43eb54b6dd9e316fa6e375420c76
bellmast <jbm444@gmail.com>
2014-02-25 17:27:58 -0600

    Update IncViz.js

--- IncViz.js
+++ IncViz.js
@@ -40,35 +40,30 @@ function drawNetwork(data) {
         textSet.push(paper.text(xPos, midY+radius+10, orgNames[i-1]))
     }
     posConcatArray = []
-    posConcatCount = [1, 1, 1, 1]
+    posConcatCount = [-1, -1, -1, -1]
     startingPosConcat = undefined
     for(i=0; i < orgNumber; i++) {
         for(h=i+1; h < orgNumber; h++) {
 
-            //startingPosX = //left of the peak
-            //startingPosY = //left of the peak
-
-            //unless we're past the first node
-
-            startingPosX = orgXcoords[i]+((orgRadii[i])*Math.sin((Math.PI/180)*(180*posConcatCount[i])))
-            startingPosY = midY+((orgRadii[i])*Math.cos((Math.PI/180)*(180*posConcatCount[i])))
+            startingPosX = orgXcoords[i]+((orgRadii[i])*Math.sin((Math.PI/180)*(60*posConcatCount[i])))
+            startingPosY = midY+((orgRadii[i])*Math.cos((Math.PI/180)*(60*posConcatCount[i])))
             if(i>0) {
                 startingPosConcat = startingPosX+startingPosY
             }
             if($.inArray(startingPosConcat, posConcatArray) != -1) {
                 posConcatCount[i] += 1
-                startingPosX = orgXcoords[i]+((orgRadii[i])*Math.sin((Math.PI/180)*(180*posConcatCount[i]))) //radius modified
-                startingPosY = midY+((orgRadii[i])*Math.cos((Math.PI/180)*(180*posConcatCount[i]))) //radius modified
+                startingPosX = orgXcoords[i]+((orgRadii[i])*Math.sin((Math.PI/180)*(60*posConcatCount[i]))) //radius modified
+                startingPosY = midY+((orgRadii[i])*Math.cos((Math.PI/180)*(60*posConcatCount[i]))) //radius modified
             }
             
-            endingPosX = orgXcoords[h]+((orgRadii[h])*Math.sin((Math.PI/180)*(180*posConcatCount[h])))
-            endingPosY = midY+((orgRadii[h])*Math.cos((Math.PI/180)*(180*posConcatCount[h])))
+            endingPosX = orgXcoords[h]+((orgRadii[h])*Math.sin((Math.PI/180)*(60*posConcatCount[h])))
+            endingPosY = midY+((orgRadii[h])*Math.cos((Math.PI/180)*(60*posConcatCount[h])))
             endingPosConcat = endingPosX+endingPosY
             
             if($.inArray(endingPosConcat, posConcatArray) != -1) {
                 posConcatCount[h] += 1
-                endingPosX = orgXcoords[h]+((orgRadii[h])*Math.sin((Math.PI/180)*(180*posConcatCount[h]))) //radius modified
-                endingPosY = midY+((orgRadii[h])*Math.cos((Math.PI/180)*(180*posConcatCount[h]))) //radius modified
+                endingPosX = orgXcoords[h]+((orgRadii[h])*Math.sin((Math.PI/180)*(60*posConcatCount[h]))) //radius modified
+                endingPosY = midY+((orgRadii[h])*Math.cos((Math.PI/180)*(60*posConcatCount[h]))) //radius modified
             }
             posConcatArray.push(endingPosConcat, startingPosConcat)
             curvePosX = (startingPosX+endingPosX)/2
