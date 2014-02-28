18dcb827f6cfe9efecf6459910009d47453b3e40
bellmast <jbm444@gmail.com>
2014-02-27 18:08:09 -0600

    Update IncViz.js

--- IncViz.js
+++ IncViz.js
@@ -228,7 +228,7 @@ function drawNetwork(data) {
                 newCurve.hide()
                 newArrow.hide()
             } else {
-                isExist = Math.log(isExist)
+                isExist = Math.log(isExist)*3
                 newCurve.attr({"stroke-width":(isExist/10)})
                 newArrow.attr({"stroke-width":(isExist/10)})
             }
@@ -330,7 +330,7 @@ function drawNetwork(data) {
                 newCurve2.hide()
                 newArrow2.hide()
             } else {
-                isExist2 = Math.log(isExist2)
+                isExist2 = Math.log(isExist2)*3
                 newCurve2.attr({"stroke-width":(isExist2/10)})
                 newArrow2.attr({"stroke-width":(isExist2/10)})
             }
