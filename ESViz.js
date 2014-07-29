396fee1e9bc830ad1962581607c701542f7ab4c7
bellmast <jbm444@gmail.com>
2014-07-29 17:18:34 -0500

    Update ESViz.js

--- ESViz.js
+++ ESViz.js
@@ -19,6 +19,8 @@ function runProgram() {
 function drawNetwork(data) {
 
 	textSet = paper.set()
+	scholarSet = paper.set()
+	linesSet = paper.set()
 
 	KJFFheight = canvasHeight/2
 	KPrizeheight = KJFFheight-((1/2)*KJFFheight)
@@ -30,12 +32,139 @@ function drawNetwork(data) {
 
 	slots = canvasWidth/(yearAxis+1)
 	yearXcoords = []
+	KJFFcounts = []
+	KJFFscaleTracker = []
+	KDFPcounts = []
+	KDFPscaleTracker = []
+
+	years = []
 
 	for(i = 1; i < yearAxis+1; i++) {
         xPos = slots*i
         yearXcoords.push(xPos)
         textSet.push(paper.text(xPos, canvasHeight-50, earliestYear+i-1))
+        years.push(earliestYear+i-1)
+        KJFFcounts.push(0)
+        KJFFscaleTracker.push(0)
+        KDFPcounts.push(0)
+        KDFPscaleTracker.push(0)
     }
+
+    ////
+
+    dataLength = data.length
+    dataRowLength = data[0].length
+
+
+    scholarDict = {}
+
+    for(i=0; i < data.length; i++) {
+    	if(data[i][1] != '') {
+
+    	} else if(data[i][2] != '') {
+    		currentYear = data[i][2]
+    		KJFFcounts[currentYear - earliestYear] += 1
+    	} else if(data[i][3] != '') {
+    		currentYear = data[i][3]
+    		KDFPcounts[currentYear - earliestYear] += 1
+    	}
+    }
+    KJFFcountsMobile = KJFFcounts
+    KDFPcountsMobile = KDFPcounts
+
+    for(i=0; i < data.length; i++) {
+    	
+    	currentName = data[i][0]
+    	scale = data[i][4]
+
+    	if(data[i][1] != '') {
+    		currentYear = data[i][1]
+    		currentProgram = "KPrize"
+    		marker = currentYear - earliestYear
+    		yPos = KPrizeheight
+    	} else if(data[i][2] != '') {
+    		currentYear = data[i][2]
+    		currentProgram = "KJFF"
+    		yPos = KJFFheight
+    		marker = currentYear - earliestYear
+    		qMax = KJFFcounts[marker]
+    		q = qMax - KJFFcountsMobile[marker]
+    		KJFFcountsMobile[marker] -= 1
+    		KJFFscaleTracker[marker] += scale
+    	} else if(data[i][3] != '') {
+    		currentYear = data[i][3]
+    		currentProgram = "KDFP"
+    		yPos = KDFPheight
+    		marker = currentYear - earliestYear
+    		qMax = KDFPcounts[marker]
+    		q = qMax - KDFPcountsMobile[marker]
+    		KDFPcountsMobile[marker] -= 1
+    		KDFPscaleTracker[marker] += scale
+    	}
+    	
+    	xPos = yearXcoords[marker]
+
+
+
+
+    	var radiix = 3*Math.cos(2*Math.PI/qMax*q)
+    	var radiiy = 3*Math.sin(2*Math.PI/qMax*q)
+    	xPos += raddix
+    	yPos += raddiy  	
+
+    	scholarSet.push(paper.circle(xPos, yPos, scale))
+
+    	for(u=5; u < dataRowLength; u++) {
+    		cName = data[i][u]
+    		if (cName == '') {
+    			break
+    		} else if (cName in scholarDict) {
+    			targetYear = scholarDict[cName][2]
+    			targetProgram = scholarDict[cname][3]
+    			// if (targetYear == currentYear && targetProgram == currentProgram) {
+	    			xTarget = scholarDict[cName][0]
+	    			yTarget = scholardict[cName][1]
+	    			linesSet.push(
+	    			paper.path("M"+xPos+" "+yPos+"L"+xTarget+" "+yTarget).attr({"stroke-width": ".2"})
+	    			)	
+    		// 	} else {
+    		// 		baseYear = Math.min(currentYear, targetYear)
+    		// 		if (baseYear == currentYear) {
+    		// 			baseProgram = currentProgram
+    		// 			topYear = targetYear
+    		// 			topProgram = targetProgram
+    		// 		} else {
+    		// 			baseProgram = targetProgram
+    		// 			topYear = currentYear
+    		// 			topProgram = currentProgram
+    		// 		}
+    		// 		newString = baseYear+baseProgram+baseYear+baseProgram
+    		// 		if(newString in eCircleConnects){
+    		// 			eCircleConnects[newString] += 1
+    		// 		} else {
+    		// 			eCircleConnects[newString] = 0
+    		// 		}
+
+    		// 		//count a point for the connection between this programyear and that one
+    		// }
+    			
+    		}
+    	}
+
+    	scholarDict[currentName] = [xPos, yPos, currentYear, currentProgram]
+
+    }
+
+    for(i=0; i < yearAxis+1; i++) {
+    	paper.circle(yearXcoords[i], KJFFheight, KJFFscaleTracker[i])
+    	// q=0
+    	// for(u=years[i]; u < latestYear+1; u++) {
+    	// 	cWidth = eCircleConnects[years[i]+"KJFF"+u+"KJFF"]
+    	// 	paper.path("M"+yearXcoords[i]+" "+KJFFheight+"L"++" "+yTarget).attr({"stroke-width": ".2"})
+    	// }
+    	paper.circle(yearXcoords[i], KDFPheight, KDFPscaleTracker[i])
+    }
+
   
 }
 
