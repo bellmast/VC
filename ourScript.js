6fdd14a420f8a64f3be83743c05c1917c93e150d
bellmast <jbm444@gmail.com>
2014-10-27 13:52:44 -0500

    Update ourScript.js

--- ourScript.js
+++ ourScript.js
@@ -9,39 +9,12 @@ $(document).ready( function() {
 /* Prepares the cv to be dynamically expandable/collapsible   */
 /**************************************************************/
 function runProgram() {
-    $('#expList').find('li:has(ul)')
-    .click( function(event) {
-        if (this == event.target) {
-            $(this).toggleClass('expanded');
-            $(this).children('ul').toggle('medium');
-        }
-        return false;
-    })
-    .addClass('collapsed')
-    .children('ul').hide();
-
-    //Create the button funtionality
-    $('#expandList')
-    .unbind('click')
-    .click( function() {
-        $('.collapsed').addClass('expanded');
-        $('.collapsed').children().show('medium');
-    })
-    $('#collapseList')
-    .unbind('click')
-    .click( function() {
-        $('.collapsed').removeClass('expanded');
-        $('.collapsed').children().hide('medium');
-    })
     paper = new Raphael(document.getElementById('canvas_container'), canvasWidth+60, canvasHeight);  
     jQuery.getJSON("sotfTestData.js", function (data)
     {drawList(data)});
 };
 
 function drawList() {
-    p = data
-    for(var question in data){
-      paper.text(canvasWidth/2, canvasHeight/2, question)
-    }
+  dataLength = data.length
     
 }
