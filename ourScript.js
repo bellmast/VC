56e2adc471edbb283b92991bd9c1ecb8d937fe2d
bellmast <jbm444@gmail.com>
2014-10-27 14:32:25 -0500

    Update ourScript.js

--- ourScript.js
+++ ourScript.js
@@ -31,7 +31,8 @@ function drawList(data) {
       widgetThickness = 0
       for (fact in data[question][stream]) {
         widgetThickness += 1
-        newPath = paper.path("M60"+(streamY+(widgetThickness*10))+"L200"+(streamY+(widgetThickness*10)))
+        paper.path("M60"+(streamY+(widgetThickness*10))+"L200"+(streamY+(widgetThickness*10)))
+        newPath = ("M60"+(streamY+(widgetThickness*10))+"L200"+(streamY+(widgetThickness*10)))
       }
       paper.text(30, streamY+(widgetThickness*10), stream).attr({"font-size":16, "text-anchor":"start"})
       streamY += 20
