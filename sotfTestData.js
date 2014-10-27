0c19eaeb52c1be6ce7d74992e324af0c609302e7
bellmast <jbm444@gmail.com>
2014-10-27 13:37:41 -0500

    Create sotfTestData.js

---
+++ sotfTestData.js
@@ -0,0 +1,43 @@
+{
+  "How does venture capital affect companies?": {
+    "Positive Effects": {
+      "VCs actively help their portfolio companies professiona        lize earlier": [
+        "Hellmann and Puri (2000)",
+        "Artificial Addition 1",
+        "Artificial Addition 2",
+        "Artificial Addition 3",
+        "Artificial Addition 4"
+        ],
+      "VC-backed companies have faster employee growth,             greater sales, and faster sales growth": [
+        "Bruno and Tyebjee (1985)",
+        "Artificial Addition 1",
+        "Artificial Addition 2",
+        "Artificial Addition 3"
+        ],
+      "VC-backed companies have faster times to market": [
+        "Hellmann and Puri (2000)",
+        "Artificial Addition 1",
+        "Artificial Addition 2",
+        "Artificial Addition 3"
+        ],
+      "VC-backed companies have an increased likelihood of an       IPO": [
+        "Shane and Stuart (2002)",
+        "Artificial Addition 1",
+        "Artificial Addition 2"
+        ],
+      "VC-backed companies have reduced IPO underpricing": [
+        "Megginson and Weiss (1991)",
+        "Artificial Addition 1"
+        ],
+      "VC-backed companies have greater post-IPO survival           rates": [
+        "Baker and Gompers (2003)"
+        ]
+    },
+    "Negative Effects": {
+      "VCs may pressure companies to IPO earlier than optimal"       : [
+        "Gompers (1996)",
+        "Lee and Wahal (2004)"
+        ]
+    }
+  }
+}
