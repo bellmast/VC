f4e576289c5437a668294807180b00e4b072c3a6
bellmast <jbm444@gmail.com>
2014-10-28 18:49:54 -0500

    Update ourScript.js

--- ourScript.js
+++ ourScript.js
@@ -81,7 +81,7 @@ function clickHandler(ourSet, ourBrace, ourText) {
             newBrace.animate({transform:"t1 "+yTransform+"s1 "+sTransform}, 500, "<>")
             newText.animate({transform:"t1 "+yTransform}, 500, "<>")
             this.animate({transform:"t1 "+yTransform+"s1 "+sTransform}, 500, "<>")
-            //stackBelow.animate({transform:"t1 "+yTransform}, 500, "<>")
+            stackBelow.animate({transform:"t1 "+yTransform}, 500, "<>")
           }
           else if (isClicked == true) {
             newBrace.animate({transform:"t1 1s1 1"}, 500, "<>")
@@ -90,7 +90,7 @@ function clickHandler(ourSet, ourBrace, ourText) {
             })
             newText.animate({transform:"t1 1"}, 500, "<>")
             this.animate({transform:"t1 1s1 1"}, 500, "<>")
-            //stackBelow.animate({transform:"t1 1"}, 500, "<>")
+            stackBelow.animate({transform:"t1 1"}, 500, "<>")
           }
           isClicked = isClicked == false ? true : false
          }
