a09968eb2a92472a221012ada9f23e98fe424445
bellmast <jbm444@gmail.com>
2014-10-30 16:47:18 -0500

    Update ourScript.js

--- ourScript.js
+++ ourScript.js
@@ -259,7 +259,7 @@ function drawList(data) {
       }
       ourStack.push(streamBrace)
       ourStackArray.push(0)
-      controllerBox = paper.rect((indent), streamY, (qBbox["width"]-(indent+maxWidth+6-25)), ((streamY+5+(widgetThickness*10))-streamY+2)).attr({"stroke-width":0})
+      controllerBox = paper.rect((indent), streamY, (qBbox["width"]-(indent+maxWidth+6-25)), ((streamY+5+(widgetThickness*10))-streamY+2)).attr({"stroke-width":1})
       controllerBox.attr({stroke: "none", fill: "#f00", "fill-opacity": 0})
       ourStack.push(controllerBox)
       ourStackArray.push(0)
