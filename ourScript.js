c64f22217d73baa390a812c05166b4b47092ad3b
bellmast <jbm444@gmail.com>
2014-10-30 17:01:17 -0500

    Update ourScript.js

--- ourScript.js
+++ ourScript.js
@@ -260,7 +260,7 @@ function drawList(data) {
       ourStack.push(streamBrace)
       ourStackArray.push(0)
       if (factTextSet[0] == undefined) {
-        controllerBox = paper.rect(indent, streamY-10, (qBbox["width"]-indent), (((widgetThickness*10))+20)).attr({"stroke-width":1})
+        controllerBox = paper.rect(indent, streamY-13, (qBbox["width"]-indent), (((widgetThickness*10))+27)).attr({"stroke-width":1})
       }
       else {
         controllerBox = paper.rect(indent, streamY, (qBbox["width"]-indent), (((widgetThickness*10))+7)).attr({"stroke-width":1})
