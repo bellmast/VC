6f966e30f167d68c3e01f1c0e174d437af3dee36
bellmast <jbm444@gmail.com>
2014-10-30 15:53:18 -0500

    Update ourScript.js

--- ourScript.js
+++ ourScript.js
@@ -170,7 +170,7 @@ function drawList(data) {
         factLength = data[question][stream][fact].length
         newFact = paper.path("M"+(indent+maxWidth+6)+" "+(streamY+(widgetThickness*10))+"L"+qBbox["width"]+" "+(streamY+(widgetThickness*10))).attr({"stroke-width":factLength, "fill":"black", "stroke-opacity":0.3})
         factSet.push(newFact)
-        var words = stream.split(" ");
+        var words = fact.split(" ");
         var tempText = "";
         var t = paper.text(indent+maxWidth+6, streamY+(widgetThickness*10)).attr({"font-size":16, "text-anchor":"start"})
         for (var i=0; i<words.length; i++) {
