4083449277e8ae78a2e95f2fffd0571ac515fe3a
bellmast <jbm444@gmail.com>
2014-10-31 17:31:20 -0500

    Update sotfTestData.js

--- sotfTestData.js
+++ sotfTestData.js
@@ -1,14 +1,14 @@
 {
   "How does venture capital affect companies?": {
     "Positive Effects": {
-      "VCs actively help their portfolio companies professionalize earlier": [
+      "VCs actively help their portfolio companies professiona        lize earlier": [
         "Hellmann and Puri (2000)",
         "Artificial Addition 1",
         "Artificial Addition 2",
         "Artificial Addition 3",
         "Artificial Addition 4"
         ],
-      "VC-backed companies have faster employee growth, greater sales, and faster sales growth": [
+      "VC-backed companies have faster employee growth,             greater sales, and faster sales growth": [
         "Bruno and Tyebjee (1985)",
         "Artificial Addition 1",
         "Artificial Addition 2",
@@ -20,7 +20,7 @@
         "Artificial Addition 2",
         "Artificial Addition 3"
         ],
-      "VC-backed companies have an increased likelihood of an IPO": [
+      "VC-backed companies have an increased likelihood of an       IPO": [
         "Shane and Stuart (2002)",
         "Artificial Addition 1",
         "Artificial Addition 2"
@@ -29,15 +29,34 @@
         "Megginson and Weiss (1991)",
         "Artificial Addition 1"
         ],
-      "VC-backed companies have greater post-IPO survival rates": [
+      "VC-backed companies have greater post-IPO survival           rates": [
         "Baker and Gompers (2003)"
         ]
     },
     "Negative Effects": {
-      "VCs may pressure companies to IPO earlier than optimal": [
+      "VCs may pressure companies to IPO earlier than optimal"       : [
         "Gompers (1996)",
         "Lee and Wahal (2004)"
         ]
     }
-  }
+  },
+    "How can entrepreneurs attract VC?": {
+      "Founding Team Attributes": {
+        "Previous startup experience increases the likelihood of funding, and results in higher valuations": [
+          "Hsu (2007)"
+          ],
+        "Higher human capital (e.g., education, skills) increases the likelihood of funding, and results in higher valuations": [
+          "Hsu (2007)"
+          ],
+        "Previous same-industry experience increases the likelihood of funding": [
+          "Shane and Stuart (2002)"
+          ]
+      },
+      "Strategies": {
+        "Companies can leverage their early accomplishments into further financing": [
+          "Hallen (2008"
+          ]
+      }
+      
+    }
 }
