2d42741f903ead8166cd3c8690529e6191239683
bellmast <jbm444@gmail.com>
2014-10-31 17:46:56 -0500

    Update ourScript.js

--- ourScript.js
+++ ourScript.js
@@ -2,7 +2,7 @@ var canvasWidth = 800;
 var canvasHeight= 700;
 var maxWidth = 80;
 var maxWidth2 = 570
-var indent = 50;
+var indent = 40;
 var ourStack = [];
 var ourStackArray = [];
 var globalY = 0
@@ -340,7 +340,7 @@ function drawList(data) {
         controllerBox = paper.rect(indent, streamY, (qWidth-indent), (((widgetThickness*10))+7)).attr({"stroke-width":0})
       }
       controllerBox.attr({stroke: "none", fill: "#f00", "fill-opacity": 0})
-      questionY = controllerBox.getBBox().y2+20
+      questionY = controllerBox.getBBox().y2+40
       ourStack.push(controllerBox)
       ourStackArray.push(0)
       controllerBox.hover(getHoverHandler(1, factSet),
