f8b8b720c3f2ffde53781142acd2014c6afac460
bellmast <jbm444@gmail.com>
2014-10-31 16:53:36 -0500

    Update ourScript.js

--- ourScript.js
+++ ourScript.js
@@ -75,8 +75,7 @@ function clickHandler(ourFirstFact, ourSet, ourBrace, ourText, ourText2, ourFirs
          var isClicked = false
          return function(){
           var setLength = newSet.length
-          var localTransform = setLength!=1 ? (setLength-1)*17 : 0
-          console.log(localTransform)
+          var localTransform = 0
           var stackBelowTransform = (localTransform*2)
           var cStackIndex = ourStack.indexOf(this)
           var totalMove = 0
